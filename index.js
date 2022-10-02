decb19289783063100798726d107c0378274bedd
Syed Mohammed <cocsmdkillet@gmail.com>
2022-10-03 03:43:15 +0530

    Used multer to accept a file and then added a POST method to display it's name , media type and it's size in bytes.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +12,12 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// multer().single("") is used to accept a single file with the name attribute "upfile". stored in req.file
+app.post('/api/fileanalyse', multer().single("upfile"), (req, res) => {
+  let {originalname, mimetype ,size} = req.file;
+  res.status(200).json({"name": originalname, "type": mimetype, "size": size + " bytes"});
+});
+// No options for multer() since we do not need to save the file on the disk.
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
